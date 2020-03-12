bcda13cd0ffcaeaa20f1eeffe4b97384dc89610a
od3ng <noprianto@polinema.ac.id>
2020-03-12 13:36:47 +0700

    tambahan library led-matrix

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-# main.py
+"""
+Modul display information
+Digunakan untuk menampilkan ketersediaan kantong parkir
+"""
+
+from machine import Pin, SPI
+import max7219, utime
+
+spi = SPI(1, baudrate=10000000, polarity=1, phase=0, sck=Pin(4), mosi=Pin(2))
+d = max7219.Max7219(32, 8, spi, Pin(5), False)
+d.brightness(5)
+d.text("^_^",0,0)
+d.show()
+utime.sleep(3)
+
+while True:
+    d.marquee("Politeknik Negeri Malang...")
+    d.marquee("Jos...")
