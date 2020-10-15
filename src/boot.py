1841892ac02a4a10b7abcc76decc925a6e853a93
od3ng <noprianto@polinema.ac.id>
2020-10-15 13:01:16 +0700

    add library mqtt

--- src/boot.py
+++ src/boot.py
@@ -1,1 +1,45 @@
 # boot.py - - runs on boot-up
+
+import ubinascii
+
+from machine import Pin, SPI
+import max7219, utime
+
+import network
+import machine
+
+# ap kampus
+ssid = "Smart Parking"
+password = "5m4rT_P4rk!Ng"
+
+gmt7 = 3600 * 7
+
+# local
+# mqtt_server = "192.168.0.103"
+mqtt_server = "raspberrypi.local"
+mqtt_port = 1883
+# public
+# mqtt_server = "parking.sinaungoding.com"
+# mqtt_port = 8095
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_sub = "parkir/rekap"
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+spi = SPI(1, baudrate=10000000, polarity=1, phase=0, sck=Pin(18), mosi=Pin(23))
+d = max7219.Max7219(32, 8, spi, Pin(5), False)
+d.brightness(5)
+d.text("^_^", 0, 0)
+d.show()
+utime.sleep(3)
+
+if not wlan.isconnected():
+    print("connecting to network...")
+    d.marquee("Connecting to "+ssid)
+    wlan.connect(ssid, password)
+    while not wlan.isconnected():
+        pass
+print('Connection successful')
+d.marquee("Connected with ip "+wlan.ifconfig()[0])
+print(wlan.ifconfig())
--- src/main.py
+++ src/main.py
@@ -3,16 +3,70 @@ Modul display information
 Digunakan untuk menampilkan ketersediaan kantong parkir
 """
 
-from machine import Pin, SPI
-import max7219, utime
+from time import *
+import boot
+import binascii
+import ntptime
+from umqttsimple import MQTTClient
+import ujson
 
-spi = SPI(1, baudrate=10000000, polarity=1, phase=0, sck=Pin(18), mosi=Pin(23))
-d = max7219.Max7219(32, 8, spi, Pin(5), False)
-d.brightness(5)
-d.text("^_^",0,0)
-d.show()
-utime.sleep(3)
+
+def sub_cb(topic, msg):
+    print((topic, msg))
+    print(msg.decode("utf-8"))
+    parsed = ujson.loads(msg.decode("utf-8"))
+    print("length {} data {}".format(len(parsed), parsed))
+    for data in parsed:
+        for key, value in data.items():
+            print("key: {} value: {}".format(key, value))
+    if topic == b'notification' and msg == b'received':
+        print('ESP received hello message')
+
+
+# MQTT
+def connect_and_subscribe():
+    print('Connecting to %s MQTT broker port %s, subscribed to %s topic' %
+          (boot.mqtt_server, boot.mqtt_port, boot.topic_sub))
+    boot.d.marquee(
+        "Connecting to %s MQTT broker port %s, subscribed to %s topic" %
+        (boot.mqtt_server, boot.mqtt_port, boot.topic_sub))
+    client = MQTTClient(boot.client_id, boot.mqtt_server, port=boot.mqtt_port)
+    client.set_callback(sub_cb)
+    client.connect()
+    client.subscribe(boot.topic_sub)
+    print('Connected to %s MQTT broker, subscribed to %s topic' %
+          (boot.mqtt_server, boot.topic_sub))
+    boot.d.marquee("Connected")
+    sleep_ms(3000)
+    return client
+
+
+def restart_and_reconnect():
+    boot.d.marquee("Error Connect " + boot.mqtt_server)
+    sleep_ms(3000)
+    print('Failed to connect to MQTT broker. Reconnecting...')
+    sleep(10)
+    machine.reset()
+
+
+# MQTT
+try:
+    client = connect_and_subscribe()
+except OSError as e:
+    restart_and_reconnect()
+
+# INIT TIME
+ntptime.settime()
+
+
+def __time2str(dt_time: tuple):
+    return "{:04d}/{:02d}/{:02d} {:02d}:{:02d}:{:02d}".format(
+        dt_time[0], dt_time[1], dt_time[2], dt_time[3], dt_time[4], dt_time[5])
+
 
 while True:
-    d.marquee("Politeknik Negeri Malang...")
-    d.marquee("Jos...")
+    tm = time()
+    NOW = localtime(tm + boot.gmt7)
+    boot.d.marquee("Smart Parking")
+    boot.d.marquee("Politeknik Negeri Malang")
+    boot.d.marquee(__time2str(NOW))
