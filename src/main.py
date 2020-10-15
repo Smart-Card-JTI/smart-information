3cae3dba9c9d43b4c3bb4139c63cc84085e90dce
od3ng <noprianto@polinema.ac.id>
2020-10-15 12:15:05 +0700

    change pin

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ Digunakan untuk menampilkan ketersediaan kantong parkir
 from machine import Pin, SPI
 import max7219, utime
 
-spi = SPI(1, baudrate=10000000, polarity=1, phase=0, sck=Pin(4), mosi=Pin(2))
+spi = SPI(1, baudrate=10000000, polarity=1, phase=0, sck=Pin(18), mosi=Pin(23))
 d = max7219.Max7219(32, 8, spi, Pin(5), False)
 d.brightness(5)
 d.text("^_^",0,0)
